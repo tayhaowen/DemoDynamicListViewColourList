95921312ae0528b792edb23289d0c3ec93c1408e
Tay Hao Wen <tay_haowen@hotmail.com>
2021-06-08 12:59:10 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20042303/demodynamiclistviewcolourlist/MainActivity.java
@@ -0,0 +1,82 @@
+package sg.edu.rp.c346.id20042303.demodynamiclistviewcolourlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etElement;
+    Button btnAdd;
+    Button btnRemove;
+    Button btnUpdate;
+    ListView lvColour;
+    EditText etIndexElement;
+    ArrayList<String> alColours = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etElement = findViewById(R.id.editTextColour);
+        btnAdd = findViewById(R.id.buttonAddItem);
+        btnRemove = findViewById(R.id.buttonRemoveItem);
+        btnUpdate = findViewById(R.id.buttonUpdateItem);
+        lvColour = findViewById(R.id.listViewColour);
+        etIndexElement = findViewById(R.id.editTextIndex);
+
+        alColours.add("Red");
+        alColours.add("Orange");
+
+        ArrayAdapter aaColour = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, alColours);
+        lvColour.setAdapter(aaColour);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.add(pos,colour);
+                aaColour.notifyDataSetChanged();
+                etElement.setText("");
+                etIndexElement.setText("");
+                Toast.makeText(MainActivity.this, "Added Successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        lvColour.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String colour = alColours.get(position);
+                Toast.makeText(MainActivity.this, colour, Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.set(pos,colour);
+                aaColour.notifyDataSetChanged();
+            }
+        });
+        btnRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+                alColours.remove(pos);
+                aaColour.notifyDataSetChanged();
+            }
+        });
+    }
+}
